d455d99dbb82b8f1de6324661caabc1de8f2a019
Miguel Beltran <m.beltrann676@gmail.com>
2023-07-12 12:51:34 -0700

    Add Movie, Read Database, Edit Movie Rating and Comment, Delete Movie

--- forms.py
+++ forms.py
@@ -7,6 +7,12 @@ class movieForm(FlaskForm):
     movie_Title = StringField(label='Name', validators=[DataRequired()])
     year = IntegerField(label='Year', validators=[DataRequired()])
     movie_Rating = FloatField(label='Rating', validators=[DataRequired()])
-    movie_Comments = StringField(label='Critic Comment', validators=[DataRequired()])
+    movie_Comments = StringField(label='Your Review', validators=[DataRequired()])
     movie_Summary = TextAreaField(label='Summary', validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class quickForm(FlaskForm):
+    new_Rating = FloatField(label='Your Rating out of 10 e.g. 6.5', validators=[DataRequired()])
+    new_Comment = StringField(label='Your Review', validators=[DataRequired()])
     submit = SubmitField("Submit")
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
+from sqlalchemy.exc import SQLAlchemyError
 from db import db, Movies
-from forms import movieForm
+from forms import movieForm, quickForm
 import requests
 
 app = Flask(__name__)
@@ -17,15 +18,56 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    # Read rows of data from Movies database and passover list of movie objects to html page
+    movie_List = Movies.query.all()
+    return render_template("index.html", movies=movie_List)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
     addingForm = movieForm()
+    if addingForm.validate_on_submit():
+        print("If statment worked")
+        # Create new data entry for Movies database
+        with app.app_context():
+            new_Movie = Movies(
+                title=addingForm.movie_Title.data,
+                year=addingForm.year.data,
+                rating=addingForm.movie_Rating.data,
+                comments=addingForm.movie_Comments.data,
+                summary=addingForm.movie_Summary.data
+            )
+            try:
+                db.session.add(new_Movie)
+                db.session.commit()
+                print("Transaction Successful ")
+            except SQLAlchemyError as error:
+                db.session.rollback()
+                print(f"Error occured during transaction: {error}")
+                return redirect(url_for('add'))
+        return redirect(url_for('home'))
     return render_template("add.html", form=addingForm)
 
 
+@app.route('/update', methods=["GET", "POST"])
+def update():
+    editForm = quickForm()
+    movie_ID = request.args.get('id')
+    movie_Selected = db.get_or_404(Movies, movie_ID)
+    if editForm.validate_on_submit():
+        movie_Selected.rating = editForm.new_Rating.data
+        movie_Selected.comments = editForm.new_Comment.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", form=editForm, movie=movie_Selected)
+
+
+@app.route("/delete/<int:id>", methods=["GET"])
+def delete(id):
+    movie_Selected = db.get_or_404(Movies, id)
+    db.session.delete(movie_Selected)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
